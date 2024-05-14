b9a32304fe44f8d3ab035da46c2009e5ef65111b
eter-kenth <eter.kenth@gmail.com>
2024-05-13 22:52:26 -0600

    corrigiendo errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eter-kenth.github.io',
-  base: 'repo-prueba'
+  base: 'portfolio'
 });
